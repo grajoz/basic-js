986e74466c13cabcbc0e5b0885db2f0b2714f1d3
grajoz <grajoz@yandex.ru>
2020-03-22 18:16:14 +0400

    feat: add

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,21 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    /*throw 'Not implemented';*/
+   if (!arr.isArray()){throw 'Not implemented'}
+
+
+for (let i=0;i<arr.length;i++){
+
+    if(i!==arr.length-1&&arr[i]=="--discard-next"){arr.splice(i, 2);}
+    if(i==arr.length-1&&arr[i]=="--discard-next"){arr.splice(i, 1);}
+
+    if(i!==0&&arr[i]=="--discard-prev"){arr.splice(i-1, 2);}
+    if(i==0&&arr[i]=="--discard-prev"){arr.splice(i, 1);}
+
+    if(i!==arr.length-1&&arr[i]=="--double-next"){arr.splice(i, 1,arr[i+1]);}
+    if(i==arr.length-1&&arr[i]=="--double-next"){arr.splice(i, 1);}
+
+    if(i!==0&&arr[i]=="--double-prev"){arr.splice(i, 1,arr[i+1]);}
+    if(i==0&&arr[i]=="--double-prev"){arr.splice(i, 1);}
+}
+return arr;
 };
