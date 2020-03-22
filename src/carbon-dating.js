6a4be85fefea795dd70dbf0de4fb8807829a5fdb
grajoz <grajoz@yandex.ru>
2020-03-23 00:31:54 +0400

    fix: carbon dading

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,13 +1,14 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
+
 module.exports = function dateSample(sampleActivity){
   if (typeof sampleActivity!=='string'){return false}
-  
+ if (sampleActivity=='0'){return false};
   sampleActivity = Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity));
   
   if (isNaN(sampleActivity)||sampleActivity<0) {return false;}
   
   let result = (sampleActivity/(0.693/HALF_LIFE_PERIOD))+1;
-  return parseInt(result);
+  return  parseInt(result);
   };
