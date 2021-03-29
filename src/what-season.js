2cac9a0c165b841039c7b50f258195cd73b1529d
grajoz <grajoz@yandex.ru>
2021-03-30 02:17:11 +0400

    feat:sizon

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,16 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
+module.exports = function getSeason( date ) {
+  //throw 'Not implemented';
+if(!date) {
+  return 'Unable to determine the time of year!'
+}else if (Object.keys(date).length)  throw new Error(); 
+
+  let season = ['winter','winter',
+                'spring', 'spring', 'spring',
+                'summer', 'summer', 'summer',
+                'autumn','autumn','autumn',
+                'winter']
+  return season[date.getMonth()]
+
+  
   // remove line with error and write your code here
 };
